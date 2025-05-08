d7423f33106e7ec9de482b29f3312ad4c5300ae7
LuisHMH <93409874+Lum12345h@users.noreply.github.com>
2025-05-08 14:44:37 -0600

    Delete utils directory

--- utils/date.js
+++
@@ -1,8 +0,0 @@
-/**
- * Returns the current date and time in ISO 8601 format (e.g., "2023-10-27T10:00:00.000Z").
- * This format is standard and easily parsable.
- * @returns {string} Current date and time string.
- */
-function getFechaActualISO() {
-    return new Date().toISOString();
-}
